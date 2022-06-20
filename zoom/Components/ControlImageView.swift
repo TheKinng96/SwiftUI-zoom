3eb9d3a889331d093e7cf9fe07f89e10eb37701b
G3N <fengyuan.yap@gmail.com>
2022-06-20 21:52:02 +0900

    added control panel

---
+++ zoom/Components/ControlImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  zoom
+//
+//  Created by Feng Yuan Yap on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+  let icon: String
+  
+  var body: some View {
+    Image(systemName: icon)
+      .font(.system(size: 36))
+  }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+  static var previews: some View {
+    ControlImageView(icon: "minus.magnifyingglass")
+      .preferredColorScheme(.dark)
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
