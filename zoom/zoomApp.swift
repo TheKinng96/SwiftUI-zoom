f6f5d5bffe38d00ce0569d59598af019b8cdd8bc
G3N <fengyuan.yap@gmail.com>
2022-06-20 20:27:53 +0900

    Initial Commit

---
+++ zoom/zoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoomApp.swift
+//  zoom
+//
+//  Created by Feng Yuan Yap on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct zoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
