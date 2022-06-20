6598c2104edc853d427131d975952a6105c91253
G3N <fengyuan.yap@gmail.com>
2022-06-20 21:29:26 +0900

    created info panel

--- zoom/Views/ContentView.swift
+++ zoom/Views/ContentView.swift
@@ -9,11 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
   // MARK: - Property
+  @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
+  
   // MARK: - Function
+  var getOpacity: Double {
+    return isAnimating ? 1 : 0
+  }
+  
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageOffset = .zero
+      imageScale = 1
+    }
+  }
+  
   // MARK: - Content
+  
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      ZStack {
+        // MARK: - Image
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(getOpacity)
+          .offset(x: imageOffset.width, y: imageOffset.height)
+          .scaleEffect(imageScale)
+        // MARK: - 1. TAP GESTURE
+          .onTapGesture(count: 2, perform: {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              resetImageState()
+            }
+          })
+        // MARK: - 2. DRAG GESTURE
+          .gesture(
+            DragGesture()
+              .onChanged { value in
+                withAnimation(.linear(duration: 1)) {
+                  imageOffset = value.translation
+                }
+              }
+              .onEnded { _ in
+                if imageScale <= 1 {
+                  resetImageState()
+                }
+              }
+          )
+        
+      } // :ZSTACK
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear {
+        withAnimation(.linear(duration: 1)) {
+          isAnimating = true
+        }
+      }
+    } // :NAVIGATION
+    .navigationViewStyle(.stack)
   }
 }
 
