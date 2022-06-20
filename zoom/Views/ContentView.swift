1aaeff44638116ac7e065a9d5814ad0914173e00
G3N <fengyuan.yap@gmail.com>
2022-06-20 20:39:32 +0900

    added icons and setup folders

---
+++ zoom/Views/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  zoom
+//
+//  Created by Feng Yuan Yap on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  // MARK: - Property
+  // MARK: - Function
+  // MARK: - Content
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+      .preferredColorScheme(.dark)
+  }
+}
