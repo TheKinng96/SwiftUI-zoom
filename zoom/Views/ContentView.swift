ae64318d4ecbefd30a8737ee9217e7929d49abfe
G3N <fengyuan.yap@gmail.com>
2022-06-20 21:59:40 +0900

    added pinch gesture

--- zoom/Views/ContentView.swift
+++ zoom/Views/ContentView.swift
@@ -66,6 +66,26 @@ struct ContentView: View {
                 }
               }
           )
+        // MARK: - 3. MAGNIFICATION
+          .gesture(
+            MagnificationGesture()
+              .onChanged{ value in
+                withAnimation(.linear(duration: 1)) {
+                  if value >= 1 && value <= 5 {
+                    imageScale = value
+                  } else if imageScale > 5 {
+                    imageScale = 5
+                  }
+                }
+              }
+              .onEnded{ _ in
+                if imageScale > 5 {
+                  imageScale = 5
+                } else if imageScale <= 1 {
+                  resetImageState()
+                }
+              }
+          )
       } // :ZSTACK
       .navigationTitle("Pinch & Zoom")
       .navigationBarTitleDisplayMode(.inline)
